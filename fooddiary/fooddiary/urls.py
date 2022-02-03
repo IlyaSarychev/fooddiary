9e2a6c1292496c7e74d0f408c2b168d58ce74be2
Ilya <sarychev_ilya@list.ru>
2022-02-03 14:33:36 +0500

    начало проекта

---
+++ fooddiary/fooddiary/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+from django.conf import settings
+
+urlpatterns = [
+    path('', lambda request: HttpResponse('Ну привет')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls'))
+    ]
