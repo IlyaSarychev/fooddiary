8860abe06be703a9453a708b9a5233925ed41858
Ilya <sarychev_ilya@list.ru>
2022-02-04 20:36:20 +0500

    исправил модель дня

--- fooddiary/diary/models.py
+++ fooddiary/diary/models.py
@@ -6,7 +6,7 @@ class Day(models.Model):
     '''День в дневнике'''
 
     user = models.ForeignKey(User, null=True, blank=True, related_name='days', on_delete=models.CASCADE)
-    date = models.DateField('Дата', auto_now_add=True)
+    date = models.DateField('Дата')
     session_key = models.CharField('Ключ сессии', max_length=40, null=True, blank=True)
 
     class Meta:
