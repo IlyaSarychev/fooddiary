c57d2af977f3c5d909f6c4e557832bb137777217
Ilya <sarychev_ilya@list.ru>
2022-02-05 19:55:01 +0500

    новые модели и поля (еда, прием пищи и др)

--- fooddiary/diary/models.py
+++ fooddiary/diary/models.py
@@ -11,3 +11,32 @@ class Day(models.Model):
 
     class Meta:
         ordering = ('-date',)
+
+
+class Meal(models.Model):
+    '''Прием пищи'''
+
+    day = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='meals')
+    time = models.TimeField('Время приема пищи', auto_now=True)
+    food = models.ManyToManyField('Food', related_name='food', through='MealFood')
+
+
+class Food(models.Model):
+    '''Еда'''
+
+    user = models.ForeignKey(User, blank=True, related_name='food', on_delete=models.CASCADE)
+    title = models.CharField('Название', max_length=150)
+    description = models.TextField('Описание', null=True, blank=True)
+    calories = models.PositiveIntegerField('Калорийность на 100г')
+    date = models.DateTimeField('Дата создания', auto_now_add=True)
+    meals = models.ManyToManyField('Meal', related_name='meals', through='MealFood')
+
+
+
+class MealFood(models.Model):
+    '''Связь m2m приемов пищи и еды'''
+    
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='meal_food')
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='meal_food')
+    grams = models.PositiveIntegerField('Количество грамм', default=100)
+    date = models.DateTimeField('Дата добавления', auto_now_add=True)
