d10677987e7027d36a15eaac09570fe34ea5fa31
Ilya <sarychev_ilya@list.ru>
2022-02-21 19:28:36 +0500

    'd'

--- fooddiary/settings.py
+++ fooddiary/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- fooddiary/wsgi.py
+++ fooddiary/wsgi.py
@@ -2,9 +2,7 @@ from email.mime import application
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fooddiary.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
